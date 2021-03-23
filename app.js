fc358f993c0088df6119ab6253901613e202e3c9
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2021-03-23 14:32:03 +0100

    add all crud

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Object = require('./models/Object');
 
 const app = express();
-app.use(bodyParser.json());
 
 mongoose.connect('mongodb+srv://LesBonsArtisans:LesBonsArtisans@cluster0.xmgzn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -18,5 +18,41 @@ app.use((req, res, next) => {
   next();
 });
 
+
+app.use(bodyParser.json());
+
+app.get('/api/object', (req, res, next) => {
+  Object.find()
+    .then(objects => res.status(200).json(objects))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/object/:id', (req, res, next) => {
+  Object.findOne({ _id: req.params.id })
+    .then(object => res.status(200).json(object))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.post('/api/object', (req, res, next) => {
+  delete req.body._id;
+  const object = new Object({
+    ...req.body
+  });
+  object.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.put('/api/object/:id', (req, res, next) => {
+  Object.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/object/:id', (req, res, next) => {
+  Object.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
