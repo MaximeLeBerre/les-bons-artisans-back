810243afb17e274e6f056e5ad49e9bb10651554e
MaximeLeBerre <70652404+MaximeLeBerre@users.noreply.github.com>
2021-03-25 12:51:56 +0100

    change running port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('./app');
 
-app.set('port', process.env.PORT || 3000 )
+app.set('port', process.env.PORT || 5000 )
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000, console.log('server running on port 3000'));
+server.listen(process.env.PORT || 5000, console.log('server running on port 5000'));
